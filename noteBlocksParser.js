a82b754fb219a5cb7d5cabee7490b33cec8dbfff
Vladimir A. Nosenko <vladimir.nosenko@gmail.com>
2025-06-12 16:28:36 +0200

    Header Levels detection fixed

--- noteBlocksParser.js
+++ noteBlocksParser.js
@@ -73,8 +73,8 @@ class noteBlocksParser {
           //-emptyLineCount = 0; // Reset empty line count
         } else {
           // But we still to finalize currentBlock if it was header with lower header level than now
-          // Add the previously collected block if the new header level is greater than or equal to the current header level
-          if (newHeaderLevel <= currentHeaderLevel) {
+          // Add the previously collected block if the new header level is greater than to the current header level
+          if (newHeaderLevel > currentHeaderLevel) {
             this.addBlock(blocks, currentBlock);
             currentBlock = null;
             currentBlock = this.createBlock(
